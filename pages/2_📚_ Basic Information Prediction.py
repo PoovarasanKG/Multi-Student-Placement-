2445fa47e7d103dcfdc719f2540631e84cf4b57c
PoovarasanKG <77930091+PoovarasanKG@users.noreply.github.com>
2023-04-12 17:29:40 +0530

    Update 2_📚_ Basic Information Prediction.py

--- pages/2_📚_ Basic Information Prediction.py
+++ pages/2_📚_ Basic Information Prediction.py
@@ -10,6 +10,13 @@ import io
 from bytesbufio import BytesBufferIO
 import requests # or https
 
+
+hide_st_style = """ <style> #MainMenu {visibility: hidden;} 
+footer {visibility: hidden;} header {visibility: hidden;} 
+</style> 
+""" 
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 mLink = 'https://github.com/PoovarasanKG/Multi-Student-Placement-/blob/main/pages/model.pkl?raw=true'
 mfile = BytesBufferIO(requests.get(mLink).content)
 basic_model = pickle.load(mfile)
