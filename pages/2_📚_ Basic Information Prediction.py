82ab2f544b0dbd91f02eb85bbbf7140707a0d5ca
PoovarasanKG <77930091+PoovarasanKG@users.noreply.github.com>
2023-04-12 16:36:46 +0530

    Update 2_📚_ Basic Information Prediction.py

--- pages/2_📚_ Basic Information Prediction.py
+++ pages/2_📚_ Basic Information Prediction.py
@@ -25,7 +25,7 @@ st.set_page_config(page_title="Basic Info Prediction", page_icon=":📚:", layou
 
 
 URI = "https://github.com/PoovarasanKG/Multi-Student-Placement-/blob/main/pages/model.pkl"
-basic_model = pickle.load(open('URI', 'rb'))
+basic_model = pickle.load(open(URI, 'rb'))
 #joblib.load(BytesIO(requests.get(URI).content))
 
 
